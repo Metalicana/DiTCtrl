aa11ab538d006055547f339ee22b363046b90044
Metalicana <alradi9923@gmail.com>
2025-11-20 16:23:23 -0500

    sss

--- sat/run_i2v_multi_prompt.sh
+++ sat/run_i2v_multi_prompt.sh
@@ -6,7 +6,7 @@ environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
 
 inference_case_config="inference_case_configs/multi_prompts/rose2.yaml"
-run_cmd="$environs python sample_video.py --base configs/cogvideox_5b_i2v.yaml configs/inference_i2v.yaml --custom-config $inference_case_config"
+run_cmd="$environs python sample_video_i2v.py --base configs/cogvideox_5b_i2v.yaml configs/inference_i2v.yaml --custom-config $inference_case_config"
 echo ${run_cmd}
 eval ${run_cmd}
 
