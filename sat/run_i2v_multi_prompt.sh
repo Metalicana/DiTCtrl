832c0becba71ca93e91b081ec01f39d8d9586409
Metalicana <alradi9923@gmail.com>
2025-11-21 05:41:40 -0500

    dekhi

--- sat/run_i2v_multi_prompt.sh
+++ sat/run_i2v_multi_prompt.sh
@@ -6,7 +6,8 @@ environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
 
 inference_case_config="inference_case_configs/multi_prompts/rose2.yaml"
-run_cmd="$environs python sample_video_i2v.py --base configs/cogvideox_5b_i2v.yaml configs/inference_i2v.yaml --custom-config $inference_case_config"
+input_image=$(grep 'input_image:' ${inference_case_config} | awk '{print $2}' | tr -d '"')
+run_cmd="$environs python sample_video_i2v.py --base configs/cogvideox_5b_i2v.yaml configs/inference_i2v.yaml --custom-config $inference_case_config --input_image $input_image"
 echo ${run_cmd}
 eval ${run_cmd}
 
